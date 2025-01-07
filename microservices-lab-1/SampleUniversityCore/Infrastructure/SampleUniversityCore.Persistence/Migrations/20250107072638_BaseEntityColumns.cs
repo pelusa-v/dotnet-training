68f9f3de835efc3fa5ff350b5eb63a50b22eca78
pelusa-v <razor0164@gmail.com>
2025-01-07 02:28:17 -0500

    Update: SampleUniversityCore BaseEntity migrations added

---
+++ microservices-lab-1/SampleUniversityCore/Infrastructure/SampleUniversityCore.Persistence/Migrations/20250107072638_BaseEntityColumns.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleUniversityCore.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseEntityColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Teachers",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Teachers",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Students",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Students",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Sections",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Sections",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CourseSections",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "CourseSections",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Courses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Courses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CourseClasses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "CourseClasses",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ClassTypes",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "ClassTypes",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "CourseSections");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "CourseSections");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "CourseClasses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "CourseClasses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "ClassTypes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "ClassTypes");
+        }
+    }
+}
