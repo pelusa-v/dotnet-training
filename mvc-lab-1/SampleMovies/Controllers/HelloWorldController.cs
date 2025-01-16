986d8352665e861782cd6ea95448deb7e6b91262
pelusa-v <razor0164@gmail.com>
2025-01-15 20:25:35 -0500

    Practicing mvc pattern

---
+++ mvc-lab-1/SampleMovies/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMovies.Controllers;
+
+// Controller: Base class with View support (used in mvc apps)
+// ControllerBase: Base class without View support (used in webapi)
+public class HelloWorldController : Controller
+{
+    public string Index()
+    {
+        return "Default action";
+    }
+
+    public string SayHello(string name, string lastName, int id = 1)
+    {
+        return $"Hello, {name} {lastName}, ID: {id}";
+    }
+}
